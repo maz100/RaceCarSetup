8e8fa03943dc52ecf990a3c5d51a25eeb9496834
Marc Ziman <marc.ziman@gmail.com>
2014-09-03 16:16:04 +0100

    Added Entities RaceTrack + CarConfiguration

---
+++ RaceCarSetup/CarConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RaceCarSetup
+{
+	public class CarConfiguration
+	{
+		public CarConfiguration ()
+		{
+		}
+
+		public int FuelCapacity {
+			get;
+			set;
+		}
+
+		public TimeSpan LapTime {
+			get;
+			set;
+		}
+
+		public int FuelConsumptionPerLap {
+			get;
+			set;
+		}
+	}
+}
+
---
+++ RaceCarSetup/RaceTrack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RaceCarSetup
+{
+	public class RaceTrack
+	{
+		public RaceTrack ()
+		{
+		}
+
+		public int LapDistance {
+			get;
+			set;
+		}
+
+		public int TotalLaps {
+			get;
+			set;
+		}
+
+		public TimeSpan PitStopTime {
+			get;
+			set;
+		}
+	}
+}
+
